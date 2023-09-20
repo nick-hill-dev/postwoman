ca1564fc99a9db01c1d807a8c88729836fbcdce6
Nicholas Hill <nick@nick-hill.com>
2023-09-20 15:47:35 +0100

    Colour the request methods.

---
+++ src/Postwoman/WpfValueConverters/RequestMethodColorSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Postwoman.WpfValueConverters;
+
+public class RequestMethodColorSelector : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string valueString)
+        {
+            return valueString switch
+            {
+                "GET" => new SolidColorBrush(Colors.Green),
+                "POST" => new SolidColorBrush(Colors.Blue),
+                "PUT" => new SolidColorBrush(Colors.DarkCyan),
+                "DELETE" => new SolidColorBrush(Colors.Red),
+                _ => Binding.DoNothing,
+            };
+        }
+        return Binding.DoNothing;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+
+}
