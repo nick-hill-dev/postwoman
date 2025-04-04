2163ac8a1f0097cf5881a957d567838fb8b74272
Nicholas Hill <nick@nick-hill.com>
2025-04-04 15:31:33 +0100

    Move collection loading to its own class.

--- src/Postwoman/Windows/MainWindow.xaml.cs
+++ src/Postwoman/Windows/MainWindow.xaml.cs
@@ -123,38 +123,7 @@ namespace Postwoman.Windows
             };
             if (dialog.ShowDialog() == true)
             {
-                var text = File.ReadAllText(dialog.FileName);
-                var collection = JsonConvert.DeserializeObject<CollectionViewModel>(text, serializationSettings);
-                foreach (var group in collection.VariableGroups)
-                {
-                    if (!string.IsNullOrEmpty(group.InheritsGroupName))
-                    {
-                        group.Inherits = collection.VariableGroups.First(g => g.Name == group.InheritsGroupName);
-                    }
-                }
-                foreach (var environment in collection.Environments)
-                {
-                    if (!string.IsNullOrEmpty(environment.ServerName))
-                    {
-                        environment.Server = collection.Servers.First(s => s.Name == environment.ServerName);
-                    }
-                    if (!string.IsNullOrEmpty(environment.VariableGroupName))
-                    {
-                        environment.VariableGroup = collection.VariableGroups.First(g => g.Name == environment.VariableGroupName);
-                    }
-                }
-
-                // Upgrade older collections which only have a single variable group
-                if (collection.Variables != null)
-                {
-                    var newVariableGroup = new VariableGroupViewModel { Name = "Variables", Variables = collection.Variables };
-                    collection.VariableGroups.Add(newVariableGroup);
-                    if (collection.Servers.Count > 0)
-                    {
-                        collection.Environments.Add(new EnvironmentViewModel { Name = "Main", Server = collection.Servers.First(), VariableGroup = newVariableGroup });
-                    }
-                    collection.Variables = null;
-                }
+                var collection = CollectionLoader.Load(dialog.FileName);
                 collections.Collections.Add(collection);
                 collections.SelectedCollection = collection;
             }
